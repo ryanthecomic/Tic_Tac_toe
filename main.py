15fd95d6b885eb73edf0cbf137799eb51c3f86d3
Rian Antônio da Silva Gaião <38548532+ryanthecomic@users.noreply.github.com>
2024-03-03 13:37:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,59 @@
-from tictactoe import *
+import requests
+import subprocess
 
-matriz = setArray()
+def signup():
+    username = input("Digite o nome de usuário: ")
+    password = input("Digite a senha: ")
 
-while(True):
-    print_board(matriz)
-    i = int(input("X Escolha posicao 1: "))
-    j = int(input("X Escolha posicao 2: "))
-    while(not set(matriz,"X",i,j)):
-        i = int(input("X NOT Escolha posicao 1: "))
-        j = int(input("X NOT Escolha posicao 2: "))
-    if(victory(matriz,"X")):
-        print("X")
-        break
-    if check_draw(matriz):
-        break
-    print_board(matriz)
-    i = int(input("O Escolha posicao 1: "))
-    j = int(input("O Escolha posicao 2: "))
-    while(not set(matriz,"O",i,j)):
-        i = int(input("X NOT Escolha posicao 1: "))
-        j = int(input("X NOT Escolha posicao 2: "))
-    if(victory(matriz,"O")):
-        print("O")
-        break
-    if check_draw(matriz):
-        break
+    data = {'username': username, 'password': password}
+    response = requests.post('http://localhost:5000/signup', json=data)
+
+    if response.status_code == 201:
+        print("Cadastro realizado com sucesso!")
+        return True
+    else:
+        print(f"Falha no cadastro: {response.json().get('error')}")
+        return False
+
+def login():
+    username = input("Digite o nome de usuário: ")
+    password = input("Digite a senha: ")
+
+    data = {'username': username, 'password': password}
+    response = requests.post('http://localhost:5000/login', json=data)
+
+    if response.status_code == 200:
+        print("Login bem-sucedido!")
+        return True
+    else:
+        print(f"Falha no login: {response.json().get('error')}")
+        return False
+
+def main():
+    while True:
+        print("1. Cadastrar")
+        print("2. Login")
+        print("3. Sair")
+        choice = input("Escolha uma opção (1, 2, 3): ")
+
+        if choice == "1":
+            if signup():
+                break
+        elif choice == "2":
+            if login():
+                start()
+                break
+        elif choice == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Escolha uma opção válida.")
+            
+def start():
+    print("Running the game...")
+    subprocess.run(["python", "src/game/init.py"])
+    
+main()         
+
+if __name__ == "__main__":
+    main()
